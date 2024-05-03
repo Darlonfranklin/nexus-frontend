1032440082aa5797f46e490a67a85034c7d1b845
Darlonfranklin <darlonfranklin88@gmail.com>
2024-05-03 19:24:03 -0300

    Authentication implementation and corrections routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,10 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import AppRoutes from "./app.routes";
 import LoginPage from "../pages/login";
 import { useAuth } from "../contexts/auth";
+import { useEffect } from "react";
 
 const AuthRoutes: React.FC = () => {
+  const navigate = useNavigate();
   const { logged } = useAuth();
+
+  useEffect(() => {
+    if (!logged) {
+      navigate("/login");
+    }
+  }, [logged, navigate]);
 
   return (
     <Routes>
